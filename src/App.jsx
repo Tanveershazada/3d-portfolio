654605796602704a6423469400b9ff46a5ff8eb2
Tanveer shah <138810220+Tanveershazada@users.noreply.github.com>
2023-12-14 19:03:35 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import Feedbacks from './components/Feedbacks';
+import Contact from './components/Contact';
+import StarCanvas from './components/StarCanvas';
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-center bg-no-repeat bg-cover bg-hero-pattern'>
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarCanvas/>
+      </div>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
